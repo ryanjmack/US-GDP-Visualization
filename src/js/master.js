220bd1babf53a2eb43dd479ac3887d868d5abb52
root <ryanjmack21@gmail.com>
2018-07-17 09:57:41 -0400

    Add labels to the axes

--- src/js/master.js
+++ src/js/master.js
@@ -4,7 +4,8 @@
 
 import { dataset } from './data.js';
 
-const axisPadding = 50;     // we want extra padding for the axes
+const yAxisPadding = 50;     // we want extra padding for the axes
+const xAxisPadding = 50;
 const nonAxisPadding = 10;  // but we don't want to waste space on sides with no axes
 const height = 400;
 const width = 800;
@@ -13,17 +14,18 @@ const width = 800;
 const svg = d3.select('main')
               .append('svg')
               .attr('width', width)
-              .attr('height', height);
+              .attr('height', height)
+              .attr('overflow', 'visible');
 
 
 // setup the scale functions
 const xScale = d3.scaleLinear()
   .domain([0, dataset.length])
-  .range([axisPadding, width - nonAxisPadding]);
+  .range([yAxisPadding, width - nonAxisPadding]);
 
 const yScale = d3.scaleLinear()
   .domain([0, 20000])
-  .range([height - axisPadding, nonAxisPadding]);
+  .range([height - yAxisPadding, nonAxisPadding]);
 
 
 // populate the graph
@@ -35,7 +37,7 @@ svg.selectAll('rect')
    .attr('y', d => yScale(d.amount))
    .attr('class', 'bar')
    .attr('width', width / dataset.length)
-   .attr('height', d => height - yScale(d.amount) - axisPadding)
+   .attr('height', d => height - yScale(d.amount) - xAxisPadding)
    .append('title')
    .text(d => `${d.year}: Quarter ${d.quarter}\n$${(d.amount).toLocaleString()} Billion`);
 
@@ -45,6 +47,8 @@ svg.selectAll('rect')
 */
 // override x axis tick labels
 const tickValues = [0, 40, 80, 120, 160, 200, 240, 281];
+
+// starts in 1947 with 4 quarters per year
 const tickLabels = tickValues.map(x => Math.floor(x / 4) + 1947);
 
 // create the x axis
@@ -54,16 +58,29 @@ const xAxis = d3.axisBottom(xScale)
   .tickSizeOuter(0); // get rid of last tick on x axis
 
 svg.append('g')
-   .attr('transform', `translate(0, ${height - axisPadding})`)
-   .attr('class', 'xAxis')
+   .attr('transform', `translate(0, ${height - xAxisPadding})`)
    .call(xAxis);
+
+// x axis title
+svg.append('text')
+  .attr('transform', 'translate(520, 400)')
+  .attr('font-size', '13')
+  .text('Source: https://fred.stlouisfed.org/series/GDP');
+
 
 // create the y axis
 const yAxis = d3.axisLeft(yScale);
+
 svg.append('g')
-   .attr('transform', `translate(${axisPadding}, 0)`)
+   .attr('transform', `translate(${yAxisPadding}, 0)`)
    .call(yAxis);
 
-// increase the axes font size
+// y axis title
+svg.append('text')
+  .attr('transform', 'translate(70, 190)rotate(270)')
+  .attr('fill', '222')
+  .text('GDP in Billions of Dollars');
+
+// increase the font size size of the labels on the axes
 svg.selectAll('g')
    .attr('font-size', '14');
